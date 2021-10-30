11c67b883a5fc08de355a43502d90375b952fba5
EmmanuelLeonIslas <72824606+EmmanuelLeonIslas@users.noreply.github.com>
2021-10-29 22:57:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/src'));
+app.use(express.static(__dirname+'/dist/emmanuel-tour-of-heroes'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/src/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/emmanuel-tour-of-heroes/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
