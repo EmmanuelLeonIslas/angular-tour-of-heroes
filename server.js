a640d3332e62ce80d362f8499496e79a4a66ee88
EmmanuelLeonIslas <72824606+EmmanuelLeonIslas@users.noreply.github.com>
2021-10-29 23:18:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/emmanuel-tour-of-heroes'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/emmanuel-tour-of-heroes/index.html'));
+app.use(express.static('./dist/angular-tour-of-heroes'));
+
+app.get('/*', (req, res) =>{
+    res.sendFile('index.html', {
+        root: 'dist/angular-heroku'
+    });
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
