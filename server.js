20c4879c3562bbee145c9dff3374ea54549f5774
EmmanuelLeonIslas <72824606+EmmanuelLeonIslas@users.noreply.github.com>
2021-10-29 22:41:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/angular-tour-of-heroes'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/angular-tour-of-heroes/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
